9dcf9017a6d843c73f209bfe27277503a1d59790
Frexium <elliot.gamache@efrei.net>
2022-12-07 16:11:30 +0100

    yes owo yes i needed that one

--- design.vhd
+++ design.vhd
@@ -10,7 +10,7 @@ port(
     Buffer_LR : in std_logic_vector(1 downto 0);
     SEL_FCT : in std_logic_vector(3 downto 0);
     SR_OUT_LR : out std_logic_vector(1 downto 0);
-    S : out std_logic_vector(7 downto 0);
+    S : out std_logic_vector(7 downto 0)
 )
 
 end UAL;
@@ -22,8 +22,7 @@ architecture UAL_arch of UAL is
     signal My_Buffer_LR : std_logic_vector(1 downto 0);
     signal My_SEL_FCT : std_logic_vector(3 downto 0);
     signal My_SR_OUT_LR : std_logic_vector(1 downto 0);
-    signal My_S : std_logic_vector(7 downto 0);
+    signal My_S : std_logic_vector(7 downto 0)
 
 begin
 
-    
